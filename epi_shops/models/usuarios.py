fe1c84efac3a9b37843a7a1ba77a5d3c892e4afb
Arthur Liszkievich <133507985+arthurliszkievich@users.noreply.github.com>
2024-10-02 21:16:14 -0300

    Criação das views, urls e forms de Login

--- epi_shops/models/usuarios.py
+++ epi_shops/models/usuarios.py
@@ -5,35 +5,32 @@ class Usuarios(AbstractUser):
     cpf = models.CharField(max_length=14, unique=True)
     telefone = models.CharField(max_length=15)
     endereco = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255, default='futebol123')
-    username = models.CharField(max_length=150, unique=True, default='admin')
     TIPO_USUARIO_CHOICES = (
         ('Cliente', 'Cliente'),
         ('Funcionario', 'Funcionario'),
         ('Gerente', 'Gerente'),
     )
     tipo_usuario = models.CharField(max_length=12, choices=TIPO_USUARIO_CHOICES, default='Cliente')
-    senha = models.CharField(max_length=255)
-    
-    # Define related_name para evitar conflitos
     groups = models.ManyToManyField(Group, verbose_name='groups', blank=True, related_name='epi_shops_usuarios_groups')
     user_permissions = models.ManyToManyField(Permission, verbose_name='user permissions', blank=True, related_name='epi_shops_usuarios_user_permissions')
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        if not self.groups.exists():
+        if not self.groups.exists():  # Só adiciona ao grupo se não estiver em nenhum
             if self.tipo_usuario == 'Cliente':
                 grupo, _ = Group.objects.get_or_create(name='Cliente')
             elif self.tipo_usuario == 'Funcionario':
                 grupo, _ = Group.objects.get_or_create(name='Funcionario')
             elif self.tipo_usuario == 'Gerente':
                 grupo, _ = Group.objects.get_or_create(name='Gerente')
+            else:
+                grupo, _ = Group.objects.get_or_create(name='Padrão') # Grupo padrão caso tipo_usuario seja inválido
             self.groups.add(grupo)
-    
+        
+
     class Meta:
         verbose_name = 'Usuario'
         verbose_name_plural = 'Usuarios'
-        
-    def _str_(self):
-        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}" # Ou self.username se preferir
