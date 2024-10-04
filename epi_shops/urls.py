69ee4bef8a9b79012e5fd0060ccea182b116c3cf
Arthur Liszkievich <133507985+arthurliszkievich@users.noreply.github.com>
2024-10-03 21:36:51 -0300

    Correção de Bugs

--- epi_shops/urls.py
+++ epi_shops/urls.py
@@ -2,17 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< Updated upstream
-    path('', views.index, name='index'),
-<<<<<<< HEAD
-    
-=======
-    path('create_user/', views.create_user, name='create_user'), 
-    path('login/', auth_views.LoginView.as_view(template_name='epi_shops/global/mtds_entrada/login.html'), name='login'),
-    path('logout/', views.logout_user, name='logout'),
-    path('cargos/', views.cargos, name='cargos'), 
-    path('shop/', views.shop, name='shop'),
-=======
     path('', views.EpiList.as_view(), name='epi_list'),
     path('<int:pk>/', views.EpiDetail.as_view(), name='epi_detail'),
     path('create/', views.EpiCreate.as_view(), name='epi_create'),
@@ -22,6 +11,4 @@ urlpatterns = [
     path('carrinho/', views.view_cart, name='view_cart'),
     path('carrinho/remover/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('carrinho/atualizar/<int:item_id>/', views.update_cart, name='update_cart'),
->>>>>>> Stashed changes
->>>>>>> development
 ]
--- epi_shops/views.py
+++ epi_shops/views.py
@@ -1,80 +1,3 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'global\index.html') 
-=======
-<<<<<<< Updated upstream
-from django.shortcuts import render, redirect, HttpResponse
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
-from django.contrib import messages
-from .models import *
-
-def index(request):
-    return render(request, 'epi_shops/global/index.html')  # Renderiza o arquivo HTML('index.html')
-
-def login_user(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, "Logado com sucesso!")  # Mensagem de sucesso
-            redirect('index.html')
-        else:
-            messages.error(request, "Usuário ou senha inválidos!")  # Mensagem de erro
-            return HttpResponse("<p>Erro ao logar</p>")  # Retornar uma resposta em caso de erro
-    
-    return render(request, 'epi_shops/global/login.html')
-
-
-def create_user(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        password_confirm = request.POST['password_confirm']
-        cpf = request.POST['cpf']  # Obter o CPF do formulário 
-        telefone = request.POST['telefone']  # Obter o telefone do formulário
-        endereco = request.POST['endereco']  # Obter o endereço do formulário
-
-        # Verificar se o CPF já existe
-        if Usuarios.objects.filter(cpf=cpf).exists():
-            messages.error(request, "CPF já cadastrado!")
-            return render(request, 'epi_shops/global/mtds_entrada/create_user.html')
-
-        if password == password_confirm:
-            user = Usuarios.objects.create_user(username=username, email=email, password=password, cpf=cpf, telefone=telefone, endereco=endereco)  # Adicionar cpf na criação
-            user.save()
-            messages.success(request, "Usuário criado com sucesso!")
-            return redirect('index')  # Redirecionar após a criação
-        else:
-            messages.error(request, "As senhas não conferem!")
-            return HttpResponse("<p>Erro ao criar usuário</p>")
-    else:
-        return render(request, 'epi_shops/global/mtds_entrada/create_user.html')
-def logout_user(request):
-    logout(request)
-    return redirect('index.html')  # Redireciona para a URL nomeada 'home'
-
-def shop(request):
-    return HttpResponse("<p>Shop</p>")
-
-def cargos(request):    
-    if request.method == 'POST':
-        nome = request.POST['nome']
-        
-        
-        cargo = Cargos.objects.create(cargo=request.POST['cargo'])
-        cargo.save()
-        return redirect('cargos.html')
-    else:
-        return render(request, 'epi_shops/global/cargos.html')
-
-=======
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -85,7 +8,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin  # Para exigir login e
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-
 
 def index(request):
     return render(request, 'epi_shops/index.html')
@@ -233,5 +155,3 @@ def update_cart(request, item_id):
     else:  # Se for GET
         form = AddToCartForm(instance=item)  # Inicializa com os dados atuais
     return render(request, 'epi_shops/update_cart.html', {'form': form, 'item': item}) # Renderiza o template
->>>>>>> Stashed changes
->>>>>>> development
